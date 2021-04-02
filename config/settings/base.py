27f33270524edec0670e35abbbb1b4b2c4bcd65b
Jeremiah David <jeremiahedavid@gmail.com>
2021-04-02 17:39:31 +0100

    fixed pip lock

--- config/settings/base.py
+++ config/settings/base.py
@@ -275,7 +275,7 @@ CSRF_COOKIE_HTTPONLY = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-browser-xss-filter
 SECURE_BROWSER_XSS_FILTER = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#x-frame-options
-X_FRAME_OPTIONS = "SAMEORIGIN"
+X_FRAME_OPTIONS = "DENY" #"SAMEORIGIN"
 
 # EMAIL
 # ------------------------------------------------------------------------------
